995c7d95bba3e04cc5afea7d1fdb78a54f05c026
EduardoUV <eduardomorales.c01@hotmail.com>
2019-06-22 20:27:48 -0500

    Posible manejo de vida y grupo de personas

--- 3Deaths.py
+++ 3Deaths.py
@@ -3,234 +3,257 @@
 import pygame
 import sys
 from pygame.locals import *
+import random
 #----------------------------------------------------------------------------
 
 # Global
 #--------------------------------------------------------------------------
-WIDTH = 1300
+WIDTH = 800    #1300
 HEIGHT = 650
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # ---------------------------------------------------------------------
 
 # Clases
 # ---------------------------------------------------------------------
 class Agentes(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, i, n, c, vida):
         #pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("imagenes/persona2.gif")
-        self.image = pygame.transform.scale(self.image,(60,60))
+        self.image = pygame.image.load("imagenes/grupo.png")
+        self.image = pygame.transform.scale(self.image,(100,50))
         self.speed = 4
+
         self.rect = self.image.get_rect()
+
+        self.i = i
+        self.n = n
+        self.c = c
+        self.vida = vida
+
         self.equipo = []
 
-    def dibujar(self, actor):
-        actor.blit(self.image, self.rect)
 
-# ---------------------------------------------------------------------
-
-# Funciones
-# ---------------------------------------------------------------------
-def load_image(filename, transparent=False):
-    image = pygame.image.load(filename)
-    image = image.convert()
+    def AgregarPersonajes(self, agentes, equipo, screen):
+        for i in range(3):
+            #print("personaje ",i," ---->", i+10)
+            agentes.equipo.append(Agentes(random.randint(1,10),random.randint(1,10),random.randint(1,10), 800))
 
 
-    #if transparent:
-        #a=0
-        #color = image.get_at((120,200))
-        #image.set_colorkey(color, RLEACCEL)
-    return image
-# ------------------------------------t---------------------------------
+    def ActualizarAtributos(self, screen, agentes, nivel):
+        #print(nivel, agentes.i, agentes.n, agentes.c, agentes.vida)
+        agentes.barras1(screen, agentes.vida)
 
-def nivelEnJuego1(screen, agentes):
-
-    nivel1 = pygame.image.load('imagenes/barda1.png')
-    #nivel1 = pygame.transform.scale(nivel1,(700,70))
-    screen.blit(nivel1,(0,50))
-    transparent = pygame.Surface((0,0),pygame.SRCALPHA)
-
-    bloque1 = pygame.Rect(260,60,100,50)
-    bloque2 = pygame.Rect(630,60,100,50)
-    bloque3 = pygame.Rect(1040,60,100,50)
-
-    pygame.draw.rect(screen,(0,0,0), transparent.get_rect())
-
-    if(bloque1.colliderect(agentes)):
-        print("\n bloque 1 nivel 1")
-
-    elif(bloque2.colliderect(agentes)):
-        print("\n bloque 2 nivel 1")
-
-    elif(bloque3.colliderect(agentes)):
-        print("\n bloque 3 nivel 1")
+        for n in agentes.equipo:
+            self.ActualizarAtributos(screen, n, nivel + "-")
 
 
-def nivelEnJuego2(screen, agentes):
+    def RestartVida(self, screen, agentes, Rvida):
+        #print(".....>",nivel,agentes.vida)
+        agentes.vida -= Rvida
+        #print(agentes.vida)
 
-    nivel2 = pygame.image.load('imagenes/barda2.png')
-    screen.blit(nivel2,(0,180))
-    transparent = pygame.Surface((0,0),pygame.SRCALPHA)
-
-    bloque1 = pygame.Rect(90,190,100,50)
-    bloque2 = pygame.Rect(420,190,100,50)
-    bloque3 = pygame.Rect(810,190,100,50)
-    bloque4 = pygame.Rect(1100,190,100,50)
-
-    pygame.draw.rect(screen,(0,0,0), transparent.get_rect())
-
-    if(bloque1.colliderect(agentes)):
-        print("\n bloque 1 nivel 2")
-
-    elif(bloque2.colliderect(agentes)):
-        print("\n bloque 2 nivel 2")
-
-    elif(bloque3.colliderect(agentes)):
-        print("\n bloque 3 nivel 2")
-
-    elif(bloque4.colliderect(agentes)):
-        print("\n bloque 4 nivel 2")
+        for n in agentes.equipo:
+            self.RestartVida(screen, n, Rvida)
 
 
-def nivelEnJuego3(screen, agentes):
+    def load_image(self, filename, transparent=False):
+        image = pygame.image.load(filename)
+        image = image.convert()
 
-    nivel3 = pygame.image.load('imagenes/barda3.png')
-    screen.blit(nivel3,(0,310))
-    transparent = pygame.Surface((0,0),pygame.SRCALPHA)
-
-    bloque1 = pygame.Rect(5,320,75,50)
-    bloque2 = pygame.Rect(265,320,75,50)
-    bloque3 = pygame.Rect(600,320,90,50)
-    bloque4 = pygame.Rect(970,320,80,50)
-    bloque5 = pygame.Rect(1230,320,100,50)
-
-    pygame.draw.rect(screen,(0,0,0), transparent.get_rect())
-
-    if(bloque1.colliderect(agentes)):
-        print("\n bloque 1 nivel 3")
-
-    elif(bloque2.colliderect(agentes)):
-        print("\n bloque 2 nivel 3")
-
-    elif(bloque3.colliderect(agentes)):
-        print("\n bloque 3 nivel 3")
-
-    elif(bloque4.colliderect(agentes)):
-        print("\n bloque 4 nivel 3")
-
-    elif(bloque5.colliderect(agentes)):
-        print("\n bloque 5 nivel 3")
-
-def barras1(screen,agentes):
-    
-    color1 = (255,0,0,0) 
-    color2 = (40, 210, 250)
-    rect1 = (10,530, 400, 10)
-    rect2 = (10,540,400,10)
-    width = 0
-
-    pygame.draw.rect(screen, color1, rect1, width)
-    pygame.draw.rect(screen, color2, rect2, width)
-
-def barras2(screen,agentes):
-    
-    color1 = (255,0,0,0)
-    rect1 = (10,570,400, 10)
-    color2 = (40, 210, 250)
-    rect2 = (10,580,400, 10)
-    width = 0
-
-    pygame.draw.rect(screen, color1, rect1, width)
-    pygame.draw.rect(screen, color2, rect2, width)
-
-def barras3(screen,agentes):
-    
-    color1 = (255,0,0,0)
-    rect1 = (10,610, 400, 10)
-    color2 = (40, 210, 250)
-    rect2 = (10,620, 400, 10)
-    width = 0
-
-    pygame.draw.rect(screen, color1, rect1, width)
-    pygame.draw.rect(screen, color2, rect2, width)
-
-def MovimientoTeclas(agentes):
-        key = pygame.key.get_pressed()
-
-        if key[K_LEFT]:
-            if agentes.rect.left == 0:
-                pass
-            else:
-                agentes.rect.left -= agentes.speed
-                if key[K_UP]:
-                    if agentes.rect.top == 0:
-                        pass
-                    else:
-                        agentes.rect.top -= agentes.speed
-
-                elif key[K_DOWN]:
-                    if agentes.rect.top == 429:
-                        pass
-                    else:
-                        agentes.rect.top += agentes.speed
-
-        elif key[K_RIGHT]:
-            if agentes.rect.left == 1240:
-                pass
-            else:
-                agentes.rect.left += agentes.speed
-                if key[K_UP]:
-                    if agentes.rect.top == 0:
-                        pass
-                    else:
-                        agentes.rect.top -= agentes.speed
-
-                elif key[K_DOWN]:
-                    if agentes.rect.top == 429:
-                        pass
-                    else:
-                        agentes.rect.top += agentes.speed
-
-        elif key[K_UP]:
-            if agentes.rect.top == 0:
-                pass
-            else:
-                agentes.rect.top -= agentes.speed
-
-        elif key[K_DOWN]:
-            if agentes.rect.top == 432:
-                pass
-            else:
-                agentes.rect.top += agentes.speed
+        return image
 
 
-def AgregarAgentes(screen, agentes, evento):
+    def nivelEnJuego1(self, screen, agentes):
 
-    if evento.type == pygame.MOUSEBUTTONDOWN:
-        if evento.button == 1:
-            print("boton izquierdo")
+        nivel1 = pygame.image.load('imagenes/barda1.png')
+        #nivel1 = pygame.transform.scale(nivel1,(700,70))
+        screen.blit(nivel1,(0,50))
+        transparent = pygame.Surface((0,0),pygame.SRCALPHA)
+
+        bloque1 = pygame.Rect(260,60,100,50)
+        bloque2 = pygame.Rect(630,60,100,50)
+        bloque3 = pygame.Rect(1040,60,100,50)
+
+        pygame.draw.rect(screen,(0,0,0), transparent.get_rect())
+
+        if(bloque1.colliderect(agentes)):
+            self.RestartVida(screen, agentes, 1)
+            print("\n bloque 1 nivel 1")
+
+        elif(bloque2.colliderect(agentes)):
+            print("\n bloque 2 nivel 1")
+
+        elif(bloque3.colliderect(agentes)):
+            print("\n bloque 3 nivel 1")
+
+    def nivelEnJuego2(self, screen, agentes):
+
+        nivel2 = pygame.image.load('imagenes/barda2.png')
+        screen.blit(nivel2,(0,180))
+        transparent = pygame.Surface((0,0),pygame.SRCALPHA)
+
+        bloque1 = pygame.Rect(90,190,100,50)
+        bloque2 = pygame.Rect(420,190,100,50)
+        bloque3 = pygame.Rect(810,190,100,50)
+        bloque4 = pygame.Rect(1100,190,100,50)
+
+        pygame.draw.rect(screen,(0,0,0), transparent.get_rect())
+
+        if(bloque1.colliderect(agentes)):
+            print("\n bloque 1 nivel 2")
+
+        elif(bloque2.colliderect(agentes)):
+            print("\n bloque 2 nivel 2")
+
+        elif(bloque3.colliderect(agentes)):
+            print("\n bloque 3 nivel 2")
+
+        elif(bloque4.colliderect(agentes)):
+            print("\n bloque 4 nivel 2")
+
+    def nivelEnJuego3(self, screen, agentes):
+
+        nivel3 = pygame.image.load('imagenes/barda3.png')
+        screen.blit(nivel3,(0,310))
+        transparent = pygame.Surface((0,0),pygame.SRCALPHA)
+
+        bloque1 = pygame.Rect(5,320,75,50)
+        bloque2 = pygame.Rect(265,320,75,50)
+        bloque3 = pygame.Rect(600,320,90,50)
+        bloque4 = pygame.Rect(970,320,80,50)
+        bloque5 = pygame.Rect(1230,320,100,50)
+
+        pygame.draw.rect(screen,(0,0,0), transparent.get_rect())
+
+        if(bloque1.colliderect(agentes)):
+            print("\n bloque 1 nivel 3")
+
+        elif(bloque2.colliderect(agentes)):
+            print("\n bloque 2 nivel 3")
+
+        elif(bloque3.colliderect(agentes)):
+            print("\n bloque 3 nivel 3")
+
+        elif(bloque4.colliderect(agentes)):
+            print("\n bloque 4 nivel 3")
+
+        elif(bloque5.colliderect(agentes)):
+            print("\n bloque 5 nivel 3")
+
+    def barras1(self, screen, vida):
+
+        color1 = (255,0,0,0)
+        rect1 = (0 ,560, vida , 20) #el tercer parametro es la vida 100 = 100%
+        pygame.draw.rect(screen, color1, rect1, 1)
+
+        #color2 = (40, 210, 250)
+        #rect2 = (10,540, e1,10)
+        #pygame.draw.rect(screen, color2, rect2, 1)
+
+    '''
+    def barras2(screen,agentes):
+
+        color1 = (255,0,0,0)
+        rect1 = (10,570,400, 10)
+        color2 = (40, 210, 250)
+        rect2 = (10,580,400, 10)
+        width = 0
+
+        pygame.draw.rect(screen, color1, rect1, width)
+        pygame.draw.rect(screen, color2, rect2, width)
+
+    def barras3(screen,agentes):
+
+        color1 = (255,0,0,0)
+        rect1 = (10,610, 400, 10)
+        color2 = (40, 210, 250)
+        rect2 = (10,620, 400, 10)
+        width = 0
+
+        pygame.draw.rect(screen, color1, rect1, width)
+        pygame.draw.rect(screen, color2, rect2, width)
+    '''
+
+    def MovimientoTeclas(self, agentes):
+            key = pygame.key.get_pressed()
+
+            if key[K_LEFT]:
+                if agentes.rect.left == 0:
+                    pass
+                else:
+                    agentes.rect.left -= agentes.speed
+                    if key[K_UP]:
+                        if agentes.rect.top == 0:
+                            pass
+                        else:
+                            agentes.rect.top -= agentes.speed
+
+                    elif key[K_DOWN]:
+                        if agentes.rect.top == 429:
+                            pass
+                        else:
+                            agentes.rect.top += agentes.speed
+
+            elif key[K_RIGHT]:
+                if agentes.rect.left == 1240:
+                    pass
+                else:
+                    agentes.rect.left += agentes.speed
+                    if key[K_UP]:
+                        if agentes.rect.top == 0:
+                            pass
+                        else:
+                            agentes.rect.top -= agentes.speed
+
+                    elif key[K_DOWN]:
+                        if agentes.rect.top == 429:
+                            pass
+                        else:
+                            agentes.rect.top += agentes.speed
+
+            elif key[K_UP]:
+                if agentes.rect.top == 0:
+                    pass
+                else:
+                    agentes.rect.top -= agentes.speed
+
+            elif key[K_DOWN]:
+                if agentes.rect.top == 432:
+                    pass
+                else:
+                    agentes.rect.top += agentes.speed
 
 
-        elif evento.button == 2:
-            print("boton enmedio")
+    def MouseClick(self, screen, agentes, evento):
 
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            if evento.button == 1:
+                print("boton izquierdo")
 
-        elif evento.button == 3:
-            print("boton derecho")
+            elif evento.button == 2:
+                #agentes.AgregarPersonajes(agentes, screen)
+                print("boton enmedio")
+
+            elif evento.button == 3:
+                print("boton derecho")
 
 
 def main():
     pygame.init()
+    agentes = Agentes(0,0,0,0)
 
-    #screen.fill((255,255,255))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
     pygame.display.set_caption("Proyecto IA - 3Deaths")
-    background = load_image('imagenes/fondo.png')
+    background = agentes.load_image('imagenes/fondo.png')
 
-    agentes = Agentes()
+    agentes.AgregarPersonajes(agentes, agentes.equipo, screen)
 
     while True:
+
+
+
         screen.blit(background,(0,0))
-        #screen.blit(agentes.image,(120,120))
+        screen.blit(agentes.image, agentes.rect)
 
         for evento in pygame.event.get():
             if evento.type == QUIT:
@@ -238,22 +261,17 @@ def main():
                 sys.exit()
                 return 0
 
-            AgregarAgentes(screen, agentes, evento)
+            agentes.MouseClick(screen, agentes, evento)
+
+        agentes.ActualizarAtributos(screen, agentes, "-")
+
+        agentes.MovimientoTeclas(agentes)
+
+        agentes.nivelEnJuego1(screen, agentes)
+        agentes.nivelEnJuego2(screen, agentes)
+        agentes.nivelEnJuego3(screen, agentes)
 
 
-
-
-
-        MovimientoTeclas(agentes)
-
-        nivelEnJuego1(screen, agentes)
-        nivelEnJuego2(screen, agentes)
-        nivelEnJuego3(screen, agentes)
-
-        agentes.dibujar(screen)
-        barras1(screen,agentes)
-        barras2(screen,agentes)
-        barras3(screen,agentes)
         pygame.display.update()
         pygame.display.flip()
 
